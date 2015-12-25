65b7b230ef15dcd3fe9114b2394613699e6c6b6d
Harry Shamansky <harry.shamansky@icloud.com>
2015-12-25 13:02:39 -0500

    Initial Commit

---
+++ QLED/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  QLED
+//
+//  Created by Harry Shamansky on 12/25/15.
+//  Copyright © 2015 Harry Shamansky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
